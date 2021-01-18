70c99a761c09dd257e3e1a19001804b015c279c4
Rohan Sawant <rohansawantct83@gmail.com>
2021-01-18 16:59:01 +0530

    Added files

---
+++ frontend/src/secured.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Keycloak from "keycloak-js";
+
+class Secured extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { Keycloak: null, authenticated: false };
+    }
+
+    componentDidMount() {
+        const keycloak = Keycloak("/keycloak.json");
+        keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+            this.setState({ keycloak: keycloak, authenticated: authenticated })
+            if (authenticated) {
+                window.accessToken = keycloak.token;
+                console.log(window.accessToken);
+            }
+        })
+    }
+
+    render() {
+        if (this.state.keycloak) {
+            if (this.state.authenticated) return (
+                <div><p>This is a keycloak-secured compoent of your application</p></div>
+            ); else return (<div> Unable to authenticate</div>);
+        } return (<div>Initializing Keycloak...</div>);
+    }
+}
+
+export default Secured;
